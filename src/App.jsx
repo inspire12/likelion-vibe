81763e65fd9cd755dad0f0b4cbba45e03956d3a1
inspire12 <ox4443@naver.com>
2025-04-25 16:53:31 +0900

    feat(vibe-2): guide popup 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 // ./src/App.jsx
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MenuBar from './components/MenuBar'
+import GuidePopup from './components/GuidePopup.jsx'
 import Home from './pages/Home'
 import Project from './pages/Project'
 import History from './pages/History'
 import Contact from './pages/Contact'
 
+
 export default function App() {
   return (
     <BrowserRouter>
       <MenuBar />
+      <GuidePopup />
       {/* 메뉴바 높이만큼 패딩 추가 */}
       <div className="pt-16">
         <Routes>
